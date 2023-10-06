12b956027259667fae823760e6ded3eaffb9cb84
Osamabuild <143334464+Osamabuild@users.noreply.github.com>
2023-10-06 20:21:33 +0500

    Commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -302,12 +302,16 @@ Cal("init", {origin:"https://cal.com"});
 
   // Analytics Code
 // Header Code
- // (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  //new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  //j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  //'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  //})(window,document,'script','dataLayer','GTM-M6L7JW5B');
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-M6L7JW5B');
   // Body Code
 <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6L7JW5B"
+<<<<<<< HEAD
 height="0" width="0" style="display:none;visibility:hidden"></iframe>
 
+=======
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+>>>>>>> parent of fd34a36 (ANALYTICS Removed)
