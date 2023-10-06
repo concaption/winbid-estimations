5e150fde6e73c9b0822f999911cdc500f372d486
Osamabuild <143334464+Osamabuild@users.noreply.github.com>
2023-10-06 20:16:40 +0500

    Analytics added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,9 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M6L7JW5B')
+
 jQuery(function ($) {
 	'use strict';
 
@@ -306,6 +312,6 @@ Cal("init", {origin:"https://cal.com"});
   //'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   //})(window,document,'script','dataLayer','GTM-M6L7JW5B');
   // Body Code
-//<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6L7JW5B"
-//height="0" width="0" style="display:none;visibility:hidden"></iframe>
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6L7JW5B"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
 
